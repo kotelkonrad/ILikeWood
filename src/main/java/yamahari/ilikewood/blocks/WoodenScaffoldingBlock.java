1e06f227860ff36fd1d19634160aa034df3bdd31
Lukas <selvaggiolukas26@googlemail.com>
2019-06-24 18:37:35 +0200

    refactor names

--- src/main/java/yamahari/ilikewood/blocks/WoodenScaffoldingBlock.java
+++ src/main/java/yamahari/ilikewood/blocks/WoodenScaffoldingBlock.java
@@ -29,18 +29,18 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class WoodenScaffoldingBlock extends Block implements IWooden, IWaterLoggable {
-    private static final VoxelShape field_220121_d;
-    private static final VoxelShape field_220122_e;
+    private static final VoxelShape STABLE_SHAPE;
+    private static final VoxelShape UNSTABLE_SHAPE;
     private static final VoxelShape field_220123_f = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
     private static final VoxelShape field_220124_g = VoxelShapes.fullCube().withOffset(0.0D, -1.0D, 0.0D);
-    public static final IntegerProperty field_220118_a;
-    public static final BooleanProperty field_220119_b;
-    public static final BooleanProperty field_220120_c;
+    public static final IntegerProperty DISTANCE;
+    public static final BooleanProperty WATERLOGGED;
+    public static final BooleanProperty BOTTOM;
     private final WoodType woodType;
 
     public WoodenScaffoldingBlock(WoodType woodType) {
         super(Block.Properties.create(Material.WOOD).sound(SoundType.SCAFFOLDING).doesNotBlockMovement().variableOpacity());
-        this.setDefaultState(this.stateContainer.getBaseState().with(field_220118_a, 7).with(field_220119_b, false).with(field_220120_c, false));
+        this.setDefaultState(this.stateContainer.getBaseState().with(DISTANCE, 7).with(WATERLOGGED, false).with(BOTTOM, false));
         this.woodType = woodType;
     }
 
@@ -50,28 +50,28 @@ public class WoodenScaffoldingBlock extends Block implements IWooden, IWaterLogg
     }
 
     @Override
-    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> p_206840_1_) {
-        p_206840_1_.add(field_220118_a, field_220119_b, field_220120_c);
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(DISTANCE, WATERLOGGED, BOTTOM);
     }
 
     @Override
-    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
+    public VoxelShape getShape(BlockState blockStateIn, IBlockReader blockReaderIn, BlockPos blockPosIn, ISelectionContext selectionContextIn) {
         boolean flag = false;
         for(Block block : Constants.SCAFFOLDINGS) {
-            if(p_220053_4_.hasItem(Item.getItemFromBlock(block))) {
+            if(selectionContextIn.hasItem(Item.getItemFromBlock(block))) {
                 flag = true;
                 break;
             }
         }
         if (!flag) {
-            return p_220053_1_.get(field_220120_c) ? field_220122_e : field_220121_d;
+            return blockStateIn.get(BOTTOM) ? UNSTABLE_SHAPE : STABLE_SHAPE;
         } else {
             return VoxelShapes.fullCube();
         }
     }
 
     @Override
-    public VoxelShape getRaytraceShape(BlockState p_199600_1_, IBlockReader p_199600_2_, BlockPos p_199600_3_) {
+    public VoxelShape getRaytraceShape(BlockState blockStateIn, IBlockReader blockReaderIn, BlockPos blockPosIn) {
         return VoxelShapes.fullCube();
     }
 
@@ -81,118 +81,118 @@ public class WoodenScaffoldingBlock extends Block implements IWooden, IWaterLogg
     }
 
     @Override
-    public boolean isReplaceable(BlockState p_196253_1_, BlockItemUseContext p_196253_2_) {
-        return p_196253_2_.getItem().getItem() == this.asItem();
+    public boolean isReplaceable(BlockState blockStateIn, BlockItemUseContext blockItemUseContextIn) {
+        return blockItemUseContextIn.getItem().getItem() == this.asItem();
     }
 
     @Override
-    public BlockState getStateForPlacement(BlockItemUseContext p_196258_1_) {
-        BlockPos lvt_2_1_ = p_196258_1_.getPos();
-        World lvt_3_1_ = p_196258_1_.getWorld();
-        int lvt_4_1_ = func_220117_a(lvt_3_1_, lvt_2_1_);
-        return this.getDefaultState().with(field_220119_b, lvt_3_1_.getFluidState(lvt_2_1_).getFluid() == Fluids.WATER).with(field_220118_a, lvt_4_1_).with(field_220120_c, this.func_220116_a(lvt_3_1_, lvt_2_1_, lvt_4_1_));
+    public BlockState getStateForPlacement(BlockItemUseContext blockItemUseContextIn) {
+        BlockPos blockPos = blockItemUseContextIn.getPos();
+        World world = blockItemUseContextIn.getWorld();
+        int distance = getDistance(world, blockPos);
+        return this.getDefaultState().with(WATERLOGGED, world.getFluidState(blockPos).getFluid() == Fluids.WATER).with(DISTANCE, distance).with(BOTTOM, this.checkUnstable(world, blockPos, distance));
     }
 
     @Override
-    public void onBlockAdded(BlockState p_220082_1_, World p_220082_2_, BlockPos p_220082_3_, BlockState p_220082_4_, boolean p_220082_5_) {
-        if (!p_220082_2_.isRemote) {
-            p_220082_2_.getPendingBlockTicks().scheduleTick(p_220082_3_, this, 1);
+    public void onBlockAdded(BlockState blockStateIn0, World worldIn, BlockPos blockPosIn, BlockState blockStateIn1, boolean p_220082_5_) {
+        if (!worldIn.isRemote) {
+            worldIn.getPendingBlockTicks().scheduleTick(blockPosIn, this, 1);
         }
 
     }
 
     @Override
-    public BlockState updatePostPlacement(BlockState p_196271_1_, Direction p_196271_2_, BlockState p_196271_3_, IWorld p_196271_4_, BlockPos p_196271_5_, BlockPos p_196271_6_) {
-        if (p_196271_1_.get(field_220119_b)) {
-            p_196271_4_.getPendingFluidTicks().scheduleTick(p_196271_5_, Fluids.WATER, Fluids.WATER.getTickRate(p_196271_4_));
+    public BlockState updatePostPlacement(BlockState blockStateIn0, Direction direction, BlockState blockStateIn1, IWorld worldIn, BlockPos blockPosIn0, BlockPos blockPosIn1) {
+        if (blockStateIn0.get(WATERLOGGED)) {
+            worldIn.getPendingFluidTicks().scheduleTick(blockPosIn0, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
         }
 
-        if (!p_196271_4_.isRemote()) {
-            p_196271_4_.getPendingBlockTicks().scheduleTick(p_196271_5_, this, 1);
+        if (!worldIn.isRemote()) {
+            worldIn.getPendingBlockTicks().scheduleTick(blockPosIn0, this, 1);
         }
 
-        return p_196271_1_;
+        return blockStateIn0;
     }
 
     @Override
-    public void tick(BlockState p_196267_1_, World p_196267_2_, BlockPos p_196267_3_, Random p_196267_4_) {
-        int lvt_5_1_ = func_220117_a(p_196267_2_, p_196267_3_);
-        BlockState lvt_6_1_ = p_196267_1_.with(field_220118_a, lvt_5_1_).with(field_220120_c, this.func_220116_a(p_196267_2_, p_196267_3_, lvt_5_1_));
-        if (lvt_6_1_.get(field_220118_a) == 7) {
-            if (p_196267_1_.get(field_220118_a) == 7) {
-                p_196267_2_.addEntity(new FallingBlockEntity(p_196267_2_, (double) p_196267_3_.getX() + 0.5D, (double) p_196267_3_.getY(), (double) p_196267_3_.getZ() + 0.5D, (BlockState) lvt_6_1_.with(field_220119_b, false)));
+    public void tick(BlockState blockStateIn, World worldIn, BlockPos blockPosIn, Random rand) {
+        int distance = getDistance(worldIn, blockPosIn);
+        BlockState blockState = blockStateIn.with(DISTANCE, distance).with(BOTTOM, this.checkUnstable(worldIn, blockPosIn, distance));
+        if (blockState.get(DISTANCE) == 7) {
+            if (blockStateIn.get(DISTANCE) == 7) {
+                worldIn.addEntity(new FallingBlockEntity(worldIn, (double) blockPosIn.getX() + 0.5D, (double) blockPosIn.getY(), (double) blockPosIn.getZ() + 0.5D, (BlockState) blockState.with(WATERLOGGED, false)));
             } else {
-                p_196267_2_.destroyBlock(p_196267_3_, true);
+                worldIn.destroyBlock(blockPosIn, true);
             }
-        } else if (p_196267_1_ != lvt_6_1_) {
-            p_196267_2_.setBlockState(p_196267_3_, lvt_6_1_, 3);
+        } else if (blockStateIn != blockState) {
+            worldIn.setBlockState(blockPosIn, blockState, 3);
         }
 
     }
 
     @Override
-    public boolean isValidPosition(BlockState p_196260_1_, IWorldReader p_196260_2_, BlockPos p_196260_3_) {
-        return func_220117_a(p_196260_2_, p_196260_3_) < 7;
+    public boolean isValidPosition(BlockState blockStateIn, IWorldReader worldReaderIn, BlockPos blockPosIn) {
+        return getDistance(worldReaderIn, blockPosIn) < 7;
     }
 
     @Override
-    public VoxelShape getCollisionShape(BlockState p_220071_1_, IBlockReader p_220071_2_, BlockPos p_220071_3_, ISelectionContext p_220071_4_) {
-        if (p_220071_4_.func_216378_a(VoxelShapes.fullCube(), p_220071_3_, true) && !p_220071_4_.isSneaking()) {
-            return field_220121_d;
+    public VoxelShape getCollisionShape(BlockState blockStateIn, IBlockReader blockReaderIn, BlockPos blockPosIn, ISelectionContext selectionContextIn) {
+        if (selectionContextIn.func_216378_a(VoxelShapes.fullCube(), blockPosIn, true) && !selectionContextIn.isSneaking()) {
+            return STABLE_SHAPE;
         } else {
-            return p_220071_1_.get(field_220118_a) != 0 && p_220071_1_.get(field_220120_c) && p_220071_4_.func_216378_a(field_220124_g, p_220071_3_, true) ? field_220123_f : VoxelShapes.empty();
+            return blockStateIn.get(DISTANCE) != 0 && blockStateIn.get(BOTTOM) && selectionContextIn.func_216378_a(field_220124_g, blockPosIn, true) ? field_220123_f : VoxelShapes.empty();
         }
     }
 
     @Override
-    public IFluidState getFluidState(BlockState p_204507_1_) {
-        return p_204507_1_.get(field_220119_b) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(p_204507_1_);
+    public IFluidState getFluidState(BlockState blockStateIn) {
+        return blockStateIn.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(blockStateIn);
     }
 
-    private boolean func_220116_a(IBlockReader p_220116_1_, BlockPos p_220116_2_, int p_220116_3_) {
-        return p_220116_3_ > 0 && !(p_220116_1_.getBlockState(p_220116_2_.down()).getBlock() instanceof WoodenScaffoldingBlock);
+    private boolean checkUnstable(IBlockReader blockReaderIn, BlockPos blockPosIn, int distance) {
+        return distance > 0 && !(blockReaderIn.getBlockState(blockPosIn.down()).getBlock() instanceof WoodenScaffoldingBlock);
     }
 
-    public static int func_220117_a(IBlockReader p_220117_0_, BlockPos p_220117_1_) {
-        BlockPos.MutableBlockPos lvt_2_1_ = (new BlockPos.MutableBlockPos(p_220117_1_)).move(Direction.DOWN);
-        BlockState lvt_3_1_ = p_220117_0_.getBlockState(lvt_2_1_);
-        int lvt_4_1_ = 7;
-        if (lvt_3_1_.getBlock() instanceof WoodenScaffoldingBlock) {
-            lvt_4_1_ = lvt_3_1_.get(field_220118_a);
-        } else if (Block.hasSolidSide(lvt_3_1_, p_220117_0_, lvt_2_1_, Direction.UP)) {
+    public static int getDistance(IBlockReader blockReaderIn, BlockPos blockPosIn) {
+        BlockPos.MutableBlockPos mutableBlockPos = (new BlockPos.MutableBlockPos(blockPosIn)).move(Direction.DOWN);
+        BlockState blockState0 = blockReaderIn.getBlockState(mutableBlockPos);
+        int distance = 7;
+        if (blockState0.getBlock() instanceof WoodenScaffoldingBlock) {
+            distance = blockState0.get(DISTANCE);
+        } else if (Block.hasSolidSide(blockState0, blockReaderIn, mutableBlockPos, Direction.UP)) {
             return 0;
         }
 
-        Iterator var5 = Direction.Plane.HORIZONTAL.iterator();
+        Iterator it = Direction.Plane.HORIZONTAL.iterator();
 
-        while (var5.hasNext()) {
-            Direction lvt_6_1_ = (Direction) var5.next();
-            BlockState lvt_7_1_ = p_220117_0_.getBlockState(lvt_2_1_.setPos(p_220117_1_).move(lvt_6_1_));
-            if (lvt_7_1_.getBlock() instanceof WoodenScaffoldingBlock) {
-                lvt_4_1_ = Math.min(lvt_4_1_, lvt_7_1_.get(field_220118_a) + 1);
-                if (lvt_4_1_ == 1) {
+        while (it.hasNext()) {
+            Direction direction = (Direction) it.next();
+            BlockState blockState1 = blockReaderIn.getBlockState(mutableBlockPos.setPos(blockPosIn).move(direction));
+            if (blockState1.getBlock() instanceof WoodenScaffoldingBlock) {
+                distance = Math.min(distance, blockState1.get(DISTANCE) + 1);
+                if (distance == 1) {
                     break;
                 }
             }
         }
 
-        return lvt_4_1_;
+        return distance;
     }
 
     static {
-        field_220118_a = BlockStateProperties.DISTANCE_0_7;
-        field_220119_b = BlockStateProperties.WATERLOGGED;
-        field_220120_c = BlockStateProperties.BOTTOM;
+        DISTANCE = BlockStateProperties.DISTANCE_0_7;
+        WATERLOGGED = BlockStateProperties.WATERLOGGED;
+        BOTTOM = BlockStateProperties.BOTTOM;
         VoxelShape lvt_0_1_ = Block.makeCuboidShape(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
         VoxelShape lvt_1_1_ = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 2.0D, 16.0D, 2.0D);
         VoxelShape lvt_2_1_ = Block.makeCuboidShape(14.0D, 0.0D, 0.0D, 16.0D, 16.0D, 2.0D);
         VoxelShape lvt_3_1_ = Block.makeCuboidShape(0.0D, 0.0D, 14.0D, 2.0D, 16.0D, 16.0D);
         VoxelShape lvt_4_1_ = Block.makeCuboidShape(14.0D, 0.0D, 14.0D, 16.0D, 16.0D, 16.0D);
-        field_220121_d = VoxelShapes.or(lvt_0_1_, new VoxelShape[]{lvt_1_1_, lvt_2_1_, lvt_3_1_, lvt_4_1_});
+        STABLE_SHAPE = VoxelShapes.or(lvt_0_1_, new VoxelShape[]{lvt_1_1_, lvt_2_1_, lvt_3_1_, lvt_4_1_});
         VoxelShape lvt_5_1_ = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 2.0D, 2.0D, 16.0D);
         VoxelShape lvt_6_1_ = Block.makeCuboidShape(14.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
         VoxelShape lvt_7_1_ = Block.makeCuboidShape(0.0D, 0.0D, 14.0D, 16.0D, 2.0D, 16.0D);
         VoxelShape lvt_8_1_ = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 2.0D);
-        field_220122_e = VoxelShapes.or(field_220123_f, new VoxelShape[]{field_220121_d, lvt_6_1_, lvt_5_1_, lvt_8_1_, lvt_7_1_});
+        UNSTABLE_SHAPE = VoxelShapes.or(field_220123_f, new VoxelShape[]{STABLE_SHAPE, lvt_6_1_, lvt_5_1_, lvt_8_1_, lvt_7_1_});
     }
 }
--- src/main/java/yamahari/ilikewood/items/WoodenScaffoldingItem.java
+++ src/main/java/yamahari/ilikewood/items/WoodenScaffoldingItem.java
@@ -18,51 +18,51 @@ import yamahari.ilikewood.blocks.WoodenScaffoldingBlock;
 import javax.annotation.Nullable;
 
 public class WoodenScaffoldingItem extends BlockItem {
-    public WoodenScaffoldingItem(Block p_i50039_1_, Properties p_i50039_2_) {
-        super(p_i50039_1_, p_i50039_2_);
+    public WoodenScaffoldingItem(Block blockIn, Properties properties) {
+        super(blockIn, properties);
     }
 
     @Nullable
     @Override
-    public BlockItemUseContext getBlockItemUseContext(BlockItemUseContext p_219984_1_) {
-        BlockPos lvt_2_1_ = p_219984_1_.getPos();
-        World lvt_3_1_ = p_219984_1_.getWorld();
-        BlockState lvt_4_1_ = lvt_3_1_.getBlockState(lvt_2_1_);
-        if (!(lvt_4_1_.getBlock() instanceof WoodenScaffoldingBlock)) {
-            return WoodenScaffoldingBlock.func_220117_a(lvt_3_1_, lvt_2_1_) == 7 ? null : p_219984_1_;
+    public BlockItemUseContext getBlockItemUseContext(BlockItemUseContext blockItemUseContextIn) {
+        BlockPos blockPos = blockItemUseContextIn.getPos();
+        World world = blockItemUseContextIn.getWorld();
+        BlockState blockState = world.getBlockState(blockPos);
+        if (!(blockState.getBlock() instanceof WoodenScaffoldingBlock)) {
+            return WoodenScaffoldingBlock.getDistance(world, blockPos) == 7 ? null : blockItemUseContextIn;
         } else {
-            Direction lvt_6_2_;
-            if (p_219984_1_.isPlacerSneaking()) {
-                lvt_6_2_ = p_219984_1_.func_221533_k() ? p_219984_1_.getFace().getOpposite() : p_219984_1_.getFace();
+            Direction direction;
+            if (blockItemUseContextIn.isPlacerSneaking()) {
+                direction = blockItemUseContextIn.func_221533_k() ? blockItemUseContextIn.getFace().getOpposite() : blockItemUseContextIn.getFace();
             } else {
-                lvt_6_2_ = p_219984_1_.getFace() == Direction.UP ? p_219984_1_.getPlacementHorizontalFacing() : Direction.UP;
+                direction = blockItemUseContextIn.getFace() == Direction.UP ? blockItemUseContextIn.getPlacementHorizontalFacing() : Direction.UP;
             }
 
-            int lvt_7_1_ = 0;
-            BlockPos.MutableBlockPos lvt_8_1_ = (new BlockPos.MutableBlockPos(lvt_2_1_)).move(lvt_6_2_);
+            int distance = 0;
+            BlockPos.MutableBlockPos mutableBlockPos = (new BlockPos.MutableBlockPos(blockPos)).move(direction);
 
-            while(lvt_7_1_ < 7) {
-                if (!lvt_3_1_.isRemote && !World.isValid(lvt_8_1_)) {
-                    PlayerEntity lvt_9_1_ = p_219984_1_.getPlayer();
-                    int lvt_10_1_ = lvt_3_1_.getHeight();
-                    if (lvt_9_1_ instanceof ServerPlayerEntity && lvt_8_1_.getY() >= lvt_10_1_) {
+            while(distance < 7) {
+                if (!world.isRemote && !World.isValid(mutableBlockPos)) {
+                    PlayerEntity lvt_9_1_ = blockItemUseContextIn.getPlayer();
+                    int lvt_10_1_ = world.getHeight();
+                    if (lvt_9_1_ instanceof ServerPlayerEntity && mutableBlockPos.getY() >= lvt_10_1_) {
                         SChatPacket lvt_11_1_ = new SChatPacket((new TranslationTextComponent("build.tooHigh", lvt_10_1_)).applyTextStyle(TextFormatting.RED), ChatType.GAME_INFO);
                         ((ServerPlayerEntity)lvt_9_1_).connection.sendPacket(lvt_11_1_);
                     }
                     break;
                 }
 
-                lvt_4_1_ = lvt_3_1_.getBlockState(lvt_8_1_);
-                if (!(lvt_4_1_.getBlock() instanceof WoodenScaffoldingBlock)) {
-                    if (lvt_4_1_.isReplaceable(p_219984_1_)) {
-                        return BlockItemUseContext.func_221536_a(p_219984_1_, lvt_8_1_, lvt_6_2_);
+                blockState = world.getBlockState(mutableBlockPos);
+                if (!(blockState.getBlock() instanceof WoodenScaffoldingBlock)) {
+                    if (blockState.isReplaceable(blockItemUseContextIn)) {
+                        return BlockItemUseContext.func_221536_a(blockItemUseContextIn, mutableBlockPos, direction);
                     }
                     break;
                 }
 
-                lvt_8_1_.move(lvt_6_2_);
-                if (lvt_6_2_.getAxis().isHorizontal()) {
-                    ++lvt_7_1_;
+                mutableBlockPos.move(direction);
+                if (direction.getAxis().isHorizontal()) {
+                    ++distance;
                 }
             }
 
